00d8c2481a7b8d878d2d976c94237f6686f591d4
martenzoe <biz.zoellner@gmail.com>
2025-06-06 17:57:06 +0200

    Restructure businessplan generator into own folder with layout and pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import DashboardPage from "./pages/DashboardPage";
+import BusinessplanIntro from "./generator/BusinessplanIntro";
 
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/businessplan" element={<BusinessplanIntro />} />
 
         {/* Später: Dashboard, FAQ, Kontakt usw. */}
       </Routes>
